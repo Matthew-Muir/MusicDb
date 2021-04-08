01833a87e88c3d37575117f8121dd04b62136f72
Matt <matthewtmuir@gmail.com>
2021-04-08 14:15:53 -0700

    Created Song Model + Library Controller

---
+++ Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicDb.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+
+        [DataType(DataType.Date)]
+        public  DateTime DateAdded { get; set; }
+    }
+}
